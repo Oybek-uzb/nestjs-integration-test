e679d2448816289a01bd8f819f5101632c707568
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-05-26 09:42:06 +0500

    modules connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import {ConfigModule} from "@nestjs/config";
+import {PrismaModule} from "./prisma/prisma.module";
+import {UserModule} from "./user/user.module";
+import {TodoModule} from "./todo/todo.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  })],
+  imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+      }),
+      PrismaModule,
+      UserModule,
+      TodoModule
+  ],
 })
 export class AppModule {}
