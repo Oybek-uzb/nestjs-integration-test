9ff467fcde4d9ebdc242c287a94f480b59981ef8
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-05-29 01:12:40 +0500

    #added dotenv-cli

--- src/todo/test/integration/todo.service.int-spec.ts
+++ src/todo/test/integration/todo.service.int-spec.ts
@@ -1,3 +1,18 @@
+import { Test } from "@nestjs/testing";
+import {PrismaService} from "../../../prisma/prisma.service";
+import {AppModule} from "../../../app.module";
+
 describe('TodoService Int', () => {
+    let prismaService: PrismaService;
+
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        prismaService = moduleRef.get(PrismaService)
+        await prismaService.cleanDatabase();
+    })
+
     it.todo('should pass')
 })
